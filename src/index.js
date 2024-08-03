8fb76cc087711ef56ff723be6dc7ae4d58c46220
Nagendra-Yadav-1 <nagendrayadav13938@gmail.com>
2024-08-03 18:22:24 +0530

    Util code data

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 import dotenv from "dotenv";
 dotenv.config();
 import connectDB from "./db/index.js";
-connectDB();
+import { app } from "./app.js";
 
+app.on("error", (error) => {
+    console.log("ERROR:", error);
+    throw error;
+});
+
+connectDB()
+    .then((res) => {
+        app.listen(process.env.PORT || 3000, () => {
+            console.log(`* Server is running at port : ${process.env.PORT}`);
+        });
+    })
+    .catch((err) => {
+        console.log("MONGO DB connect !!!", err);
+    });
+
+    
 
 // import mongoose from "mongoose";
 // import { DB_NAME } from "./constant.js";
@@ -11,10 +27,10 @@ connectDB();
 // const app = express()(async () => {
 //     try {
 //         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-//         app.on("error", (error) => {
-//             console.log("ERROR:", error);
-//             throw error;
-//         });
+// app.on("error", (error) => {
+//     console.log("ERROR:", error);
+//     throw error;
+// });
 
 //         app.listen(process.env.PORT, () => {
 //             console.log(`Server is running on the ${process.env.PORT}`);
